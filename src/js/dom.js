604ba1433b9552758dc2165bd5db184f6f3687de
aaronfraint <aaronfraint@icloud.com>
2021-12-27 16:54:30 -0500

    add input element

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,7 +1,11 @@
 const button_to_select_tazs = document.getElementById("button-to-select-tazs");
 const button_to_clear_tazs = document.getElementById("button-to-clear-tazs");
-const button_to_analyze_tazs = document.getElementById(
-  "button-to-analyze-tazs"
-);
+const button_to_save_tazs = document.getElementById("button-to-save-tazs");
+const zone_name_input = document.getElementById("input-zone-name");
 
-export { button_to_select_tazs, button_to_clear_tazs, button_to_analyze_tazs };
+export {
+  button_to_select_tazs,
+  button_to_clear_tazs,
+  button_to_save_tazs,
+  zone_name_input,
+};
