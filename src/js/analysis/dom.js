39449854abbdeb2b0569c1eec54ba9dea0cb052b
aaronfraint <aaronfraint@icloud.com>
2022-01-18 15:04:37 -0500

    add graph element

--- src/js/analysis/dom.js
+++ src/js/analysis/dom.js
@@ -2,5 +2,6 @@ const button_to_select_another_destination = document.getElementById(
   "button-to-select-another-destination"
 );
 const button_to_make_zone = document.getElementById("button-to-make-zone");
+const graph = document.getElementById("graph");
 
-export { button_to_select_another_destination, button_to_make_zone };
+export { button_to_select_another_destination, button_to_make_zone, graph };
